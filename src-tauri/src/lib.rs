ca0e512e7dc2097a94e75cbad043f9b13c3d8f88
Sharan Karthikeyan <karthikeyansharan00@gmail.com>
2026-06-28 18:22:03 -0500

    feat: review_session command returning a SessionDiff

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,7 +11,8 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             commands::start_session,
-            commands::cleanup_session
+            commands::cleanup_session,
+            commands::review_session
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
